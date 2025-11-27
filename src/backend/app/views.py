0d9465106a76a26b9a7f36b799f1c8270706c9c1
darylscl <165374218+darylscl@users.noreply.github.com>
2025-11-27 10:07:06 +0800

    Completed instructor exam module CRUD with test
    * added codeowners
    
    * added dummy codeowners
    
    * added dummy codeowners
    
    * add only codeowners
    
    * Completed exam moduels model and models testing
    
    * sovle testing failed issue
    
    * solve testing failed issue: datetime wrong library
    
    * Solved no migrations issue result to fail test
    
    * delete cache files for consistency
    
    * Created view backend processing and simple frontend exam form
    
    * solve error commit
    
    * Created backend logic (views.py) with TDD and BDD test
    
    * test pull request
    
    * Add automated BDD test
    
    * Completed instructor exam module CRUD with test

--- src/backend/app/views.py
+++ src/backend/app/views.py
@@ -5,6 +5,9 @@ from django.urls import reverse
 from django.contrib.auth.models import User #default django admin user for now, waiting for user
 from .models import *
 
+#To see different page for different user roles
+# Login to django admin first
+# Instructor: Username: instructor Password: 1234
 
 # Helper function
 def homepage(request):
@@ -27,51 +30,47 @@ def get_default_user():
 # @login_required
 # @user_passes_test(is_instructor)
 def exam_list(request):
-    exams = Exam.objects.filter(created_by=request.user).order_by("-created_at")
-    return render(
-        request, "app/instructor/exam_list.html", {"exams": exams}
-    )  # url need to change later when frontend comes in
+    user = get_default_user()
+    exams = Exam.objects.filter(created_by=user).order_by("-created_at")
+    return render(request, "app/instructor/exam_list.html", {"exams": exams})
+ # url need to change later when frontend comes in
 
 
 # @login_required
 # @user_passes_test(is_instructor)
 def exam_create(request):
+    user = get_default_user()
     exam = None
     exam_id = request.GET.get("exam_id")
+
     if exam_id:
-        exam = Exam.objects.filter(exam_id=exam_id).first()
+        exam = Exam.objects.filter(exam_id=exam_id, created_by=user).first()
 
+    # CREATE EXAM
     if request.method == "POST" and "create_exam" in request.POST:
-        title = request.POST.get("title")
-        description = request.POST.get("description")
-        start_time = request.POST.get("start_time")
-        end_time = request.POST.get("end_time")
-
         exam = Exam.objects.create(
-            title=title,
-            description=description,
-            start_time=start_time,
-            end_time=end_time,
-            created_by=get_default_user(),
+            title=request.POST.get("title"),
+            description=request.POST.get("description"),
+            start_time=request.POST.get("start_time"),
+            end_time=request.POST.get("end_time"),
+            created_by=user,
         )
-
         return redirect(f"/instructor/exams/create/?exam_id={exam.exam_id}")
 
+    # ADD QUESTION
     if request.method == "POST" and "add_question" in request.POST:
-        exam = Exam.objects.get(exam_id=exam_id)
-        question_text = request.POST.get("question_text")
-        question_type = request.POST.get("question_type")
+        exam = Exam.objects.get(exam_id=exam_id, created_by=user)
 
         question = ExamQuestion.objects.create(
             exam=exam,
-            question_text=question_text,
-            question_type=question_type,
+            question_text=request.POST.get("question_text"),
+            question_type=request.POST.get("question_type"),
         )
 
-        if question_type == "MCQ":
+        # MCQ Choices
+        if question.question_type == "MCQ":
             choices = request.POST.getlist("choice_text")
             correct = request.POST.get("correct_choice")
-
             for idx, text in enumerate(choices):
                 Choice.objects.create(
                     choice_id=question,
@@ -82,111 +81,166 @@ def exam_create(request):
         return redirect(f"/instructor/exams/create/?exam_id={exam.exam_id}")
 
     questions = exam.questions.all() if exam else []
+    return render(request, "app/instructor/exam_form.html", {"exam": exam, "questions": questions})
 
-    return render(request, "app/instructor/exam_form.html", {
-        "exam": exam,
-        "questions": questions
-    })
 
 
 
 # @login_required
 # @user_passes_test(is_instructor)
 def exam_detail(request, exam_id):
-    exam = get_object_or_404(Exam, exam_id=exam_id, created_by=request.user)
-    questions = exam.questions.all().order_by("order_no")
-    return render(
-        request,
-        "app/instructor/exam_detail.html",
-        {"exam": exam, "questions": questions},
-    )
+    user = get_default_user()
+    exam = get_object_or_404(Exam, exam_id=exam_id, created_by=user)
+    questions = exam.questions.prefetch_related("choices").order_by("order_no")
+
+    return render(request, "app/instructor/exam_detail.html", {
+        "exam": exam, "questions": questions
+    })
 
 
 # @login_required
 # @user_passes_test(is_instructor)
 def exam_update(request, exam_id):
-    exam = get_object_or_404(Exam, exam_id=exam_id, created_by=request.user)
+    user = get_default_user()
+    exam = get_object_or_404(Exam, exam_id=exam_id, created_by=user)
 
     if request.method == "POST":
-        exam.title = request.POST.get("title") or exam.title
-        exam.description = request.POST.get("description") or ""
-        exam.start_time = request.POST.get("start_time") or exam.start_time
-        exam.end_time = request.POST.get("end_time") or exam.end_time
+        exam.title = request.POST.get("title", exam.title)
+        exam.description = request.POST.get("description", exam.description)
+        exam.start_time = request.POST.get("start_time", exam.start_time)
+        exam.end_time = request.POST.get("end_time", exam.end_time)
         exam.save()
         return redirect("instructor_exam_detail", exam_id=exam.exam_id)
 
-    return render(request, "app/instructor/exam_form.html", {"exam": exam})
+    return render(request, "app/instructor/exam_edit.html", {"exam": exam})
+
+
 
 
 # @login_required
 # @user_passes_test(is_instructor)
 def exam_delete(request, exam_id):
-    exam = get_object_or_404(Exam, exam_id=exam_id, created_by=request.user)
+    user = get_default_user()
+    exam = get_object_or_404(Exam, exam_id=exam_id, created_by=user)
+    exam.delete()
+    return redirect("instructor_exam_list")
 
-    if request.method == "POST":
-        exam.delete()
-        return redirect("instructor_exam_list")
 
-    return render(request, "app/instructor/exam_confirm_delete.html", {"exam": exam})
 
 
 # @login_required
 # @user_passes_test(is_instructor)
 def question_create(request, exam_id):
-    exam = get_object_or_404(Exam, exam_id=exam_id, created_by=request.user)
+    user = get_default_user()
+    exam = get_object_or_404(Exam, exam_id=exam_id, created_by=user)
 
     if request.method == "POST":
-        text = request.POST.get("question_text")
-        q_type = request.POST.get("question_type")
-        order_no = request.POST.get("order_no") or 1
-
-        if text and q_type:
-            ExamQuestion.objects.create(
-                exam - exam,
-                text=text,
-                q_type=q_type,
-                order_no=order_no,
-            )
-            return redirect("instructor_exam_detail", exam_id=exam.exam_id)
+        ExamQuestion.objects.create(
+            exam=exam,
+            question_text=request.POST.get("question_text"),
+            question_type=request.POST.get("question_type"),
+            order_no=request.POST.get("order_no") or 1,
+        )
+        return redirect("instructor_exam_detail", exam_id=exam.exam_id)
 
     return render(request, "app/instructor/question_form.html", {"exam": exam})
 
 
 # @login_required
 # @user_passes_test(is_instructor)
-def question_delete(request, exam_id, question_id):
-    exam = get_object_or_404(Exam, exam_id=exam_id, created_by=request.user)
-    question = get_object_or_404(ExamQuestion, id=question_id, exam=exam)
+def question_update(request, question_id):
+    question = get_object_or_404(ExamQuestion, id=question_id)
+    exam = question.exam
 
     if request.method == "POST":
-        question.delete()
+        old_type = question.question_type
+        new_type = request.POST.get("question_type")
+
+        question.question_text = request.POST.get("question_text")
+        question.question_type = new_type
+        question.save()
+
+        if old_type == "TEXT" and new_type == "MCQ":
+            question.choices.all().delete()
+
+            choices = [
+                request.POST.get("choice_text_0"),
+                request.POST.get("choice_text_1"),
+                request.POST.get("choice_text_2"),
+                request.POST.get("choice_text_3"),
+            ]
+            correct = int(request.POST.get("correct_choice", 0))
+
+            for i, text in enumerate(choices):
+                Choice.objects.create(
+                    choice_id=question,
+                    choice_text=text,
+                    is_correct=(i == correct),
+                )
+
+        elif old_type == "MCQ" and new_type == "TEXT":
+            question.choices.all().delete()
+
+        elif new_type == "MCQ":
+            choices_qs = list(question.choices.all())
+
+            for i, choice in enumerate(choices_qs):
+                choice.choice_text = request.POST.get(f"choice_text_{i}")
+                choice.is_correct = (str(i) == request.POST.get("correct_choice"))
+                choice.save()
+
         return redirect("instructor_exam_detail", exam_id=exam.exam_id)
 
     return render(
         request,
-        "app/instructor/question_confirm_delete.html",
-        {"exam": exam, "question": question},
+        "app/instructor/question_form.html",
+        {"question": question}
     )
+
+
+
+
+# @login_required
+# @user_passes_test(is_instructor)
+def question_delete(request, exam_id, question_id):
+    user = get_default_user()
+    exam = get_object_or_404(Exam, exam_id=exam_id, created_by=user)
+    question = get_object_or_404(ExamQuestion, id=question_id, exam=exam)
+
+    question.delete()
+    return redirect("instructor_exam_detail", exam_id=exam.exam_id)
+
 
 
 # @login_required
 # @user_passes_test(is_instructor)
 def choice_add(request, question_id):
-    question = get_object_or_404(ExamQuestion, id=question_id, exam__created_by=request.user)
+    question = get_object_or_404(ExamQuestion, id=question_id)
 
     if request.method == "POST":
-        text = request.POST.get("choice_text")
-        is_correct = bool(request.POST.get("is_correct"))
-
-        if text:
-            Choice.objects.create(
-                choice_id=question,
-                choice_text=text,
-                is_correct=is_correct,
-            )
+        Choice.objects.create(
+            choice_id=question,
+            choice_text=request.POST.get("choice_text"),
+            is_correct=bool(request.POST.get("is_correct")),
+        )
         return redirect("instructor_exam_detail", exam_id=question.exam.exam_id)
 
     return render(request, "app/instructor/choice_form.html", {"question": question})
+
+
+# @login_required
+# @user_passes_test(is_instructor)
+def choice_update(request, choice_id):
+    choice = get_object_or_404(Choice, id=choice_id)
+
+    if request.method == "POST":
+        choice.choice_text = request.POST.get("choice_text")
+        choice.is_correct = bool(request.POST.get("is_correct"))
+        choice.save()
+
+        return redirect("instructor_exam_detail", exam_id=choice.choice_id.exam.exam_id)
+
+    return render(request, "app/instructor/choice_form.html", {"choice": choice})
 
 
 #@login_required
@@ -208,6 +262,13 @@ def take_exam(request, exam_id):
         student=request.user,
         defaults={},
     )
+    
+    if attempt.submitted:
+        return render(request, "app/student/exam_already_done.html", {
+            "exam": exam,
+            "attempt": attempt
+        })
+
     
     questions = exam.questions.all().order_by("order_no")
     
